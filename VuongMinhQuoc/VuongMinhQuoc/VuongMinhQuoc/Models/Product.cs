e70b2a7ab77de5222e504236554bc82ff7647414
antv60287 <antv60287@gmail.com>
2013-07-07 08:02:47 +0000

    [Edit]

--- VuongMinhQuoc/VuongMinhQuoc/VuongMinhQuoc/Models/Product.cs
+++ VuongMinhQuoc/VuongMinhQuoc/VuongMinhQuoc/Models/Product.cs
@@ -12,9 +12,10 @@ namespace VuongMinhQuoc.Models
         public string HostName { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
-        public byte TypeId { get; set; }
+        public int Year { get; set; }
+        public int TypeId { get; set; }
         public virtual ICollection<ProductImages> Imanges { get; set; }
+        public virtual ProductType Type { get; set; }
 
     }
 }
