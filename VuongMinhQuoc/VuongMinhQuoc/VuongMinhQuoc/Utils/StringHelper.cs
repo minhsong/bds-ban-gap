efa1de2a7209a84c9048d733b218db1954a512ca
antv60287 <antv60287@gmail.com>
2013-07-07 10:53:00 +0000

    [Edit]

---
+++ VuongMinhQuoc/VuongMinhQuoc/VuongMinhQuoc/Utils/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VuongMinhQuoc.Utils
+{
+    public static class StringHelper
+    {
+        public static string StripHTML(string inputString)
+        {
+            return System.Text.RegularExpressions.Regex.Replace
+              (inputString, "<.*?>", string.Empty);
+        }
+    }
+}
